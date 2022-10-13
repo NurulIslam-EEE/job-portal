00541388b2a65a2b56bee362bbe86c7b2636bfa7
NurulIslam-EEE <nurul.islam02310@gmail.com>
2022-10-13 23:06:49 +0600

    update .env

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ console.log('Database connection is successful' )
 });
 
 // server
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`App is running on port ${port}`);
